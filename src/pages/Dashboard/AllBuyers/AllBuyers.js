91a508abe2d436766ef9c8b76ecf86899bc05e02
aminul <accounreate@gmail.com>
2022-12-02 23:29:10 +0600

    admin route added

--- src/pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import toast from "react-hot-toast";
 
 const AllBuyers = () => {
+   const { data: buyers, refetch } = useQuery({
+      queryKey: ["users/buyers"],
+      queryFn: async () => {
+         const res = await fetch("http://localhost:5000/users/buyers");
+         const data = res.json();
+         return data;
+      },
+   });
+   const handleDelete = (id) => {
+      const confirmation = window.confirm("Are you sure to Delete it?");
+      if (confirmation) {
+         fetch(`http://localhost:5000/users/buyer/${id}`, {
+            method: "DELETE",
+         })
+            .then((res) => res.json())
+            .then((data) => {
+               if (data.deletedCount > 0) {
+                  toast.success("Delete Successful");
+                  refetch();
+               }
+            });
+      }
+   };
    return (
       <div>
-         <h2 className="text-5xl text-red-500">All Buyers</h2>
+         <h2 className="text-4xl text-red-500 mb-4">All Buyers</h2>
+         <div className="mr-3">
+            <div className="overflow-x-auto w-full">
+               <table className="table w-full">
+                  <thead>
+                     <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Delete</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {buyers?.map((buyer) => {
+                        return (
+                           <tr key={buyer._id}>
+                              <td>{buyer?.name}</td>
+                              <td>{buyer?.email}</td>
+                              <td>
+                                 <button className="btn btn-error btn-xs px-2" onClick={() => handleDelete(buyer._id)}>
+                                    Delete
+                                 </button>
+                              </td>
+                           </tr>
+                        );
+                     })}
+                  </tbody>
+               </table>
+            </div>
+         </div>
       </div>
    );
 };
--- src/pages/Dashboard/AllSellers/AllSellers.js
+++ src/pages/Dashboard/AllSellers/AllSellers.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import toast from "react-hot-toast";
 
 const AllSellers = () => {
+   const { data: seller, refetch } = useQuery({
+      queryKey: ["users/seller"],
+      queryFn: async () => {
+         const res = await fetch("http://localhost:5000/users/seller");
+         const data = res.json();
+         return data;
+      },
+   });
+   const handleDelete = (id) => {
+      const confirmation = window.confirm("Are you sure to Delete it?");
+      if (confirmation) {
+         fetch(`http://localhost:5000/users/singleSeller/${id}`, {
+            method: "DELETE",
+         })
+            .then((res) => res.json())
+            .then((data) => {
+               if (data.deletedCount > 0) {
+                  toast.success("Delete Successful");
+                  refetch();
+               }
+            });
+      }
+   };
+
    return (
       <div>
-         <h2 className="text-5xl text-red-500">All Sellers</h2>
+         <h2 className="text-4xl text-red-500 mb-4">All Sellers</h2>
+         <div className="mr-3">
+            <div className="overflow-x-auto w-full">
+               <table className="table w-full">
+                  <thead>
+                     <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Verify</th>
+                        <th>Delete</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {seller?.map((singleSeller) => {
+                        return (
+                           <tr key={singleSeller._id}>
+                              <td>{singleSeller?.name}</td>
+                              <td>{singleSeller?.email}</td>
+                              <td>
+                                 <button className={`btn btn-xs px-4`}>Verify</button>
+                              </td>
+                              <td>
+                                 <button className="btn btn-error btn-xs px-2" onClick={() => handleDelete(singleSeller._id)}>
+                                    Delete
+                                 </button>
+                              </td>
+                           </tr>
+                        );
+                     })}
+                  </tbody>
+               </table>
+            </div>
+         </div>
       </div>
    );
 };
